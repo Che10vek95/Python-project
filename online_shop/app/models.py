a77ee863dc4d7175df0fedc4265a1955f478dec8
Che10vek95 <atamanvanya7@gmail.com>
2025-12-06 21:19:04 +0200

    Lesson 3 hm first part not updated shop db

--- online_shop/app/models.py
+++ online_shop/app/models.py
@@ -11,8 +11,12 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    status = db.Column(db.Boolean, nullable=True, default=True)
+    category = db.Column(db.String(50), nullable=True)
+    rating = db.Column(db.Float, nullable=True, default=0.0)
+    sale = db.Columndb.Column(db.Boolean, nullable=True, default=False)
