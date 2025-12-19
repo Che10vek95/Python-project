a9e18a16c5c63eaf38251b4d1e91a6c2b8c93e87
Che10vek95 <atamanvanya7@gmail.com>
2025-12-19 18:54:58 +0200

    Pre-cristmas homework

--- online_shop/app/routes.py
+++ online_shop/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import db, Product
 
 # Define Blueprint
@@ -13,3 +13,19 @@ def index():
 def products():
     products = Product.query.all()
     return render_template('product_list.html', products=products)
+
+@bp.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        author = request.form['author']
+        # description = request.form['description']
+        price = request.form['price']
+        qantity = request.form['quantity']
+        product = Product(name=name, author=author, price=float(price), quantity=int(qantity))
+        db.session.add(product)
+        db.session.commit()
+        flash('Product added successfully!')
+        return redirect(url_for('routes.products'))
+    
+    return render_template('product_form.html', action ='Add', product=None)
