bf6658b08f7a8088fc1f8fb2a5e58ed27d2fb30b
Che10vek95 <atamanvanya7@gmail.com>
2026-01-31 21:38:51 +0200

    Lesson 5 homework

--- online_shop/app/routes.py
+++ online_shop/app/routes.py
@@ -29,3 +29,33 @@ def add_product():
         return redirect(url_for('routes.products'))
     
     return render_template('product_form.html', action ='Add', product=None)
+
+@bp.route('/delete/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    flash('Product deleted!')
+    return redirect(url_for('routes.products'))
+
+
+
+@bp.route('/update/<int:product_id>', methods=['GET', 'POST'])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.author = request.form['author']
+        product.description=request.form['description']
+        product.price = float(request.form['price'])
+        product.quantity = int(request.form['quantity'])
+        product.category = request.form['category']
+        
+        
+      
+        db.session.commit()
+        flash('Product updated!')
+        return redirect(url_for('routes.products'))
+    return render_template('product_form.html', action='Update', product=product)
+
+
